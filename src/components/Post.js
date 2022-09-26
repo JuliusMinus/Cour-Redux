67b6321b3b1c927bf575f3e8ef707a2fe473c217
JulienPetit <julienpetit1@gmail.com>
2022-09-26 12:05:20 +0200

    first commit

---
+++ src/components/Post.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { isEmpty } from "./Utils";
+import { useDispatch, useSelector } from "react-redux";
+import Like from "./Like";
+import { deletePost, editPost } from "../actions/post.action";
+
+const Post = ({ post }) => {
+  const [editToogle, setEditToogle] = useState(false);
+  const [editContent, setEditContent] = useState(post.content);
+
+  const user = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+
+    const postData = {
+      title: post.title,
+      author: user[0].pseudo,
+      content: editContent,
+      likes: post.likes,
+      id: post.id,
+    };
+
+    dispatch(editPost(postData));
+    setEditToogle(false);
+  };
+
+  return (
+    <div className="post">
+      {!isEmpty(user[0]) && user[0].pseudo === post.author && (
+        <div className="edit-delete">
+          <img
+            onClick={() => setEditToogle(!editToogle)}
+            src="./icons/edit.svg"
+            alt="edit"
+          />
+          <img
+            src="./icons/delete.svg"
+            alt="delete"
+            onClick={() => dispatch(deletePost(post.id))}
+          />
+        </div>
+      )}
+
+      <h2>{post.title}</h2>
+      <img
+        src="https://picsum.photos/1500/400"
+        className="post-img"
+        alt="img-post"
+      />
+
+      {editToogle ? (
+        <form onSubmit={(e) => handleEdit(e)}>
+          <textarea
+            defaultValue={post.content}
+            onChange={(e) => setEditContent(e.target.value)}
+          ></textarea>
+          <input type="submit" value="Valider les modifs" />
+        </form>
+      ) : (
+        <p>{post.content}</p>
+      )}
+
+      <div className="author">
+        <h5>{post.author}</h5>
+        <Like post={post} />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
